4a1e9275d6a92dfc1f7e602ba538141d1db9a059
Alexander Koch <kochalexander@gmx.net>
2024-04-18 10:20:17 +0200

    Initial commit

---
+++ train.py
@@ -0,0 +1,272 @@
+import jax
+from jax import jit, random, numpy as jnp
+import numpy as np
+import optax
+from einops import rearrange
+from unet import UNet
+import math
+import argparse
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import utils
+from typing import NamedTuple, Optional, Any, Iterable
+from tqdm import tqdm
+from grain.python import DataLoader, SequentialSampler, ReadOptions, MapTransform, Batch, ShardOptions, IndexSampler
+from einops import reduce, repeat
+from functools import partial
+import cloudpickle
+import pickle
+
+def cycle(dl: Iterable[Any]) -> Any:
+    while True:
+        it = iter(dl)
+        for x in it:
+            yield x
+
+def logsnr_schedule_cosine(t: jax.Array, logsnr_min: float =-15, logsnr_max: float =15) -> jax.Array:
+    t_min = math.atan(math.exp(-0.5 * logsnr_max))
+    t_max = math.atan(math.exp(-0.5 * logsnr_min))
+    return -2 * jnp.log(jnp.tan(t_min + t * (t_max - t_min)))
+
+def right_pad_dims_to(x: jax.Array, t: jax.Array) -> jax.Array:
+    padding_dims = x.ndim - t.ndim
+    if padding_dims <= 0:
+        return t
+    return t.reshape(*t.shape, *((1,) * padding_dims))
+
+def q_sample(x_start: jax.Array, times: jax.Array, noise: jax.Array):
+    log_snr = logsnr_schedule_cosine(times)
+    log_snr_padded = right_pad_dims_to(x_start, log_snr)
+    alpha, sigma = jnp.sqrt(jax.nn.sigmoid(log_snr_padded)), jnp.sqrt(jax.nn.sigmoid(-log_snr_padded))
+    x_noised = x_start * alpha + noise * sigma
+    return x_noised, log_snr
+ 
+def p_sample(model_fn, key, x, time, time_next, **kwargs):
+    log_snr = logsnr_schedule_cosine(time)
+    log_snr_next = logsnr_schedule_cosine(time_next)
+    c = -jnp.expm1(log_snr - log_snr_next)
+
+    squared_alpha, squared_alpha_next = jax.nn.sigmoid(log_snr), jax.nn.sigmoid(log_snr_next)
+    squared_sigma, squared_sigma_next = jax.nn.sigmoid(-log_snr), jax.nn.sigmoid(-log_snr_next)
+
+    alpha, sigma, alpha_next = map(
+        jnp.sqrt, (squared_alpha, squared_sigma, squared_alpha_next)
+    )
+
+    batch_log_snr = repeat(log_snr, " -> b", b=x.shape[0])
+    pred = model_fn(x, time=batch_log_snr, **kwargs)
+
+    x_start = jnp.clip(alpha * x - sigma * pred, -1, 1)
+    model_mean = alpha_next * (x * (1 - c) / alpha + c * x_start)
+    posterior_variance = squared_sigma_next * c
+
+    noise = random.normal(key, x.shape)
+    return jax.lax.cond(time_next == 0, lambda m: m, lambda m: m + jnp.sqrt(posterior_variance) * noise, model_mean)
+
+
+class SliceDS(Dataset):
+    def __init__(self, path):
+        self.tensor = torch.load(path)
+        self.src = self.tensor["src"].cpu().numpy()
+        self.tgt = self.tensor["tgt"].cpu().numpy()
+
+    def __len__(self):
+        return self.src.shape[0]
+
+    def __getitem__(self, index):
+        src_slice, tgt_slice = self.src[index], self.tgt[index]
+        return src_slice.reshape(256, 256, 1), tgt_slice.reshape(256, 256, 1)
+
+
+def tof_transform(x):
+    # assumes x is not an empty image
+    x = x.reshape(-1, 256 * 256)
+    upper_bound = jnp.quantile(x, 0.999, axis=1).reshape(-1, 1)
+    x = jnp.clip(x, a_min=0)
+    x = jnp.minimum(x, upper_bound) / upper_bound
+    x = x.reshape(-1, 256, 256, 1)
+    return x
+
+def cta_transform(x):
+    # [-50, 350]
+    background_mask = (x == 0)
+    out = (jnp.clip(x, -50, 350) + 50) / 400
+    out = jnp.where(background_mask, 0., out)
+    return out
+
+class TrainingState(NamedTuple):
+    params: dict
+    opt_state: optax.OptState
+    train_loss: jax.Array
+    val_loss: jax.Array
+    key: jax.Array
+
+def main(args):
+    module = UNet(128, channels=1)
+    opt = optax.adam(1e-4)
+
+    src_transform = tof_transform
+    tgt_transform = cta_transform
+    min_snr_gamma = int(args.min_snr_gamma)
+    validate_every_n_steps = int(args.validate_every_n_steps)
+
+    @jit
+    def loss(params, batch):
+        x, y = batch
+        x = src_transform(x) * 2 - 1
+        y = tgt_transform(y) * 2 - 1
+
+        y_hat = module.apply(params, x)
+        loss = jnp.mean(jnp.square(y-y_hat))
+        return loss
+
+    @jit
+    def diff_loss(params, batch, keys):
+        x, y = batch
+        x = src_transform(x) * 2 - 1
+        y = tgt_transform(y) * 2 - 1
+
+        times = random.uniform(keys[0], (y.shape[0],))
+        x_start = y
+
+        noise = random.normal(keys[1], x_start.shape)
+        x_noised, log_snr = q_sample(x_start=x_start, times=times, noise=noise)
+        model_out = module.apply(params, x_noised, time=log_snr, condition=x)
+
+        padded_log_snr = right_pad_dims_to(x_noised, log_snr)
+        alpha, sigma = (
+            jnp.sqrt(jax.nn.sigmoid(padded_log_snr)),
+            jnp.sqrt(jax.nn.sigmoid(-padded_log_snr)),
+        )
+        target = alpha * noise - sigma * x_start
+
+        loss = jnp.square(model_out - target)
+        loss = reduce(loss, "b ... -> b", "mean")
+
+        snr = jnp.exp(log_snr)
+        clipped_snr = jnp.clip(snr, a_max=min_snr_gamma)
+        loss_weight = clipped_snr / (snr + 1)
+        loss = (loss * loss_weight).mean()
+        return loss
+    
+    @jit
+    def sample(params, key, batch_size=4, num_sample_steps=100, **kwargs):
+        keys = random.split(key, num_sample_steps+1)
+
+        img = random.normal(keys[0], (batch_size, 256, 256, 1))
+        steps = jnp.linspace(1.0, 0.0, num_sample_steps+1)
+        xs = jnp.stack((steps[:-1], steps[1:]), axis=1)
+        model_fn = partial(module.apply, params)
+
+        def f(carry, x):
+            img, index = carry
+            time, time_next = x
+            img = p_sample(model_fn, keys[index+1], img, time, time_next, **kwargs)
+            return (img, index+1), img
+
+        # "always scan when you can!"
+        init = (img, 0)
+        (img, _), process = jax.lax.scan(f, init=init, xs=xs)
+
+        img = jnp.clip(img, -1, 1)
+        process = jnp.clip(process, -1, 1)
+        return img, process
+
+    @jit
+    def update(state, batch):
+        keys = random.split(state.key, 3)
+        next_state_key = keys[0]
+
+        train_loss_value, grads = jax.value_and_grad(diff_loss)(state.params, batch, keys[1:])
+        updates, opt_state = opt.update(grads, state.opt_state)
+        params = optax.apply_updates(state.params, updates)
+        return TrainingState(params, opt_state, train_loss_value, state.val_loss, next_state_key)
+
+    train_ds = SliceDS("../modality/data/train_slices.pt")
+    val_ds = SliceDS("../modality/data/val_slices.pt")
+
+    # Setup dataloaders
+    shard_opts = ShardOptions(0, 1)
+    read_opts = ReadOptions(num_threads=16, prefetch_buffer_size=1)
+    train_sampler = IndexSampler(len(train_ds), shard_opts, shuffle=True, seed=42)
+    val_sampler = IndexSampler(len(val_ds), shard_opts, shuffle=True, seed=42)
+    train_dl = DataLoader(data_source=train_ds,
+            sampler=train_sampler,
+            worker_count=0,
+            shard_options=shard_opts,
+            read_options=read_opts,
+            operations=[Batch(batch_size=4, drop_remainder=False)])
+    val_dl = DataLoader(data_source=train_ds,
+            sampler=val_sampler,
+            worker_count=0,
+            shard_options=shard_opts,
+            read_options=read_opts,
+            operations=[Batch(batch_size=4, drop_remainder=False)])
+    key = random.key(0)
+
+    # Setup state
+    if args.load is not None:
+        with open(args.load, "rb") as f:
+            state = pickle.load(f)
+    else:
+        x,y = next(iter(train_dl))
+        dummy_time = jnp.array([0.0])
+        initial_params = module.init(key, x, time=dummy_time, condition=y)
+        initial_opt_state = opt.init(initial_params)
+        state = TrainingState(initial_params, initial_opt_state, None, None, key)
+
+    if args.train:
+        train_length = math.ceil(len(train_ds) / 4)
+        total_steps = train_length * 400
+
+        train_dl = cycle(train_dl)
+        for step in (p := tqdm(range(total_steps))):
+            batch = next(train_dl)
+            state = update(state, batch)
+            p.set_description(f"train_loss: {state.train_loss}, val_loss: {state.val_loss}")
+
+            if step % validate_every_n_steps == validate_every_n_steps-1:
+                x,y = next(iter(val_dl))
+
+                x = src_transform(x) * 2 - 1
+                y = tgt_transform(y) * 2 - 1
+                y_hat, process = sample(state.params, key, condition=x)
+
+                val_loss = jnp.mean(jnp.square(y - y_hat))
+                state = TrainingState(state.params, state.opt_state, state.train_loss, val_loss, state.key)
+                
+                if args.save is not None:
+                    with open(args.save, "wb") as f:
+                        cloudpickle.dump(state, f)
+    elif args.sample:
+        x,y = next(iter(val_dl))
+
+        x = src_transform(x) * 2 - 1
+        y = tgt_transform(y) * 2 - 1
+        y_hat, process = sample(state.params, key, condition=x)
+
+        samples = jnp.concatenate((x,y,y_hat), axis=0)
+        samples = jnp.clamp((samples+1) * 0.5, 0., 1.)
+        samples = torch.from_numpy(np.array(samples)).reshape(-1, 1, 256, 256)
+        utils.save_image(utils.make_grid(samples, nrow=4, normalize=False, padding=1, pad_value=1.0), "out.png")
+
+        process = rearrange(process, "t b h w c -> b t h w c")
+        process = process[0]
+        indices = jnp.arange(100)
+
+        process = torch.from_numpy(np.array(process)).reshape(-1, 1, 256, 256)
+        utils.save_image(utils.make_grid(process, nrow=25, normalize=False, padding=1, pad_value=1.0), "process.png")
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    p.add_argument("--load", type=str, help="path to load pretrained weights from")
+    p.add_argument("--save", type=str, help="path to save weight to", default="checkpoint.pkl")
+    p.add_argument("--train", action="store_true", help="train the model")
+    p.add_argument("--sample", action="store_true", help="sample the model")
+    p.add_argument("--validate_every_n_steps", type=int, default=1000, help="validate the model every n steps")
+    p.add_argument("--min_snr_gamma", type=float, default=5.0, help="set loss weighting for min snr")
+    p.add_argument("--bfloat16", action="store_true", help="use bfloat16 precision")
+    main(p.parse_args())
---
+++ unet.py
@@ -0,0 +1,186 @@
+import jax
+import jax.numpy as jnp
+import math
+from einops import rearrange
+from typing import NamedTuple, Optional, Any
+from functools import partial
+from flax import linen as nn
+
+
+class SinusoidalPositionEmbeddings(nn.Module):
+    dim: int
+
+    @nn.compact
+    def __call__(self, time: jax.Array) -> jax.Array:
+        half_dim = self.dim // 2
+        embeddings = math.log(10000) / (half_dim - 1)
+        embeddings = jnp.exp(jnp.arange(half_dim) * -embeddings)
+        embeddings = time[:, None] * embeddings[None, :]
+        embeddings = jnp.concatenate((jnp.sin(embeddings), jnp.cos(embeddings)), axis=1)
+        return embeddings
+
+
+class Block(nn.Module):
+    dim_out: int
+    groups: int = 8
+
+    @nn.compact
+    def __call__(
+        self, x: jax.Array, scale_shift: Optional[list[jax.Array]] = None
+    ) -> jax.Array:
+        x = nn.Conv(features=self.dim_out, kernel_size=(3, 3), strides=1, padding=1)(x)
+        x = nn.GroupNorm(num_groups=self.groups)(x)
+
+        if scale_shift is not None:
+            scale, shift = scale_shift
+            x = x * (scale + 1) + shift
+
+        return nn.gelu(x)
+
+
+class ResNetBlock(nn.Module):
+    dim: int
+    dim_out: int
+    time_emb_dim: Optional[int] = None
+    groups: int = 8
+
+    @nn.compact
+    def __call__(self, x: jax.Array, time_emb: Optional[jax.Array] = None) -> jax.Array:
+        scale_shift = None
+        if time_emb is not None:
+            t = nn.Sequential(
+                [
+                    nn.Dense(self.dim_out * 2),
+                    nn.GroupNorm(num_groups=self.groups),
+                    nn.gelu,
+                    nn.Dense(self.dim_out * 2),
+                ]
+            )(time_emb)
+            t = rearrange(t, "b c -> b 1 1 c")
+            scale_shift = jnp.split(t, 2, axis=-1)
+
+        h = Block(self.dim_out, self.groups)(x, scale_shift=scale_shift)
+        h = Block(self.dim_out, self.groups)(h)
+        if self.dim != self.dim_out:
+            h = h + nn.Dense(self.dim_out)(x)
+        return h
+
+
+class Upsample(nn.Module):
+    dim: int
+
+    @nn.compact
+    def __call__(self, x: jax.Array) -> jax.Array:
+        s = (x.shape[0], x.shape[1] * 2, x.shape[2] * 2, x.shape[3])
+        x = jax.image.resize(x, s, method="nearest")
+        x = nn.Conv(features=self.dim, kernel_size=(3, 3), strides=1, padding=1)(x)
+        return x
+
+
+Downsample = partial(nn.Conv, kernel_size=(4, 4), strides=2, padding=1)
+
+
+class PixelShuffleUpsample(nn.Module):
+    dim: int
+
+    @nn.compact
+    def __call__(self, x: jax.Array) -> jax.Array:
+        x = nn.Sequential([nn.Dense(self.dim * 4), nn.gelu])(x)
+        x = rearrange(x, "b h w (c s1 s2) -> b (h s1) (w s2) c", s1=2, s2=2)
+        return x
+
+
+class PixelShuffleDownsample(nn.Module):
+    dim: int
+
+    @nn.compact
+    def __call__(self, x: jax.Array) -> jax.Array:
+        x = rearrange(x, "b (h s1) (w s2) c -> b h w (c s1 s2)", s1=2, s2=2)
+        x = nn.Dense(self.dim)(x)
+        return x
+
+
+class UNet(nn.Module):
+    dim: int
+    dim_mults: tuple[int, ...] = (1, 2, 4)
+    channels: int = 3
+    resnet_block_groups: int = 8
+    out_dim: Optional[int] = None
+
+    @nn.compact
+    def __call__(
+        self,
+        x: jax.Array,
+        time: Optional[jax.Array] = None,
+        condition: Optional[jax.Array] = None,
+    ) -> jax.Array:
+        init_dim = self.dim // 3 * 2
+        dims = [init_dim, *map(lambda m: self.dim * m, self.dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+        num_resolutions = len(in_out)
+
+        if time is not None:
+            time_emb_dim = self.dim * 4
+            time_embed = nn.Sequential(
+                [
+                    SinusoidalPositionEmbeddings(self.dim),
+                    nn.Dense(time_emb_dim),
+                    nn.GroupNorm(num_groups=32),
+                    nn.gelu,
+                    nn.Dense(time_emb_dim),
+                    nn.GroupNorm(num_groups=32),
+                    nn.gelu,
+                ]
+            )(time)
+        else:
+            time_emb_dim = None
+            time_embed = None
+
+        if condition is not None:
+            x = jnp.concatenate((condition, x), axis=3)
+
+        x = nn.Conv(features=init_dim, kernel_size=7, padding=3)(x)
+
+        h = []
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+            x = ResNetBlock(
+                dim_in, dim_out, time_emb_dim, self.resnet_block_groups
+            )(x, time_embed)
+            x = ResNetBlock(
+                dim_out, dim_out, time_emb_dim, self.resnet_block_groups
+            )(x, time_embed)
+            h.append(x)
+
+            if not is_last:
+                x = PixelShuffleDownsample(dim_out)(x)
+
+        mid_dim = dims[-1]
+        x = ResNetBlock(
+            mid_dim, mid_dim, time_emb_dim, self.resnet_block_groups
+        )(x, time_embed)
+
+        for ind, (dim_in, dim_out) in enumerate(reversed(in_out[1:])):
+            h_l = h.pop()
+            x = jnp.concatenate((x, h_l), axis=3)
+
+            is_last = ind >= (num_resolutions - 1)
+            x = ResNetBlock(
+                dim_out * 2, dim_in, time_emb_dim, self.resnet_block_groups
+            )(x, time_embed)
+            x = ResNetBlock(
+                dim_in, dim_in, time_emb_dim, self.resnet_block_groups
+            )(x, time_embed)
+
+            if not is_last:
+                x = PixelShuffleUpsample(dim_in)(x)
+
+        out_dim = self.out_dim if self.out_dim is not None else self.channels
+
+        x = nn.Sequential(
+            [
+                ResNetBlock(self.dim, self.dim, self.resnet_block_groups),
+                nn.Dense(out_dim),
+            ]
+        )(x)
+        return x
