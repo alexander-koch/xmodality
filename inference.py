7881d43a548fec390fe1936001d34a0b6f81558e
Alexander Koch <kochalexander@gmx.net>
2024-06-11 12:54:31 +0200

    Update inference script

--- inference.py
+++ inference.py
@@ -3,19 +3,21 @@ from jax import random, vmap
 import jax.numpy as jnp
 import pickle
 from uvit import UViT
+from adm import ADM
+from unet import UNet
 import numpy as np
 import matplotlib.pyplot as plt
 from train import sample
 import nibabel as nib
 from tqdm import tqdm
 import math
+import argparse
 from einops import rearrange
 
 def transform(img):
     #img = np.maximum(0, img)
     #max_v = np.quantile(img, 0.999)
     #return img / max_v
-    
     min_v = img.min()
     max_v = img.max()
     return (img - min_v) / (max_v - min_v)
@@ -23,22 +25,33 @@ def transform(img):
 def vmap_transform(img):
     return vmap(transform)(img)
 
-def main():
-    dtype = jnp.bfloat16
-    module = UViT(dim=128, channels=1, dtype=dtype)
-    with open("weights/uvit.pkl", "rb") as f:
+def main(args):
+    dtype = jnp.bfloat16 if args.bfloat16 else jnp.float32
+    if args.arch == "adm":
+        module = ADM(dim=128, channels=1, dtype=dtype)
+    elif args.arch == "uvit":
+        module = UViT(dim=128, channels=1, dtype=dtype)
+    elif args.arch == "unet":
+        module = UNet(dim=128, channels=1, dtype=dtype)
+        raise NotImplementedError()
+    assert args.load.endswith(".pkl")
+    print(args)
+
+    with open(args.load, "rb") as f:
         state = pickle.load(f)
 
-    path = "/fast/work/users/kochal_c/modality_data/final/topcow_mr_whole_016_0000_Warped.nii.gz"
-    out_path = "test.nii.gz"
+    if args.input.endswith(".nii.gz"):
+        run(module, state.params, not args.disable_diffusion, args.input, args.output, args.batch_size)
+    elif args.input.endswith(".txt"):
+        raise NotImplementedError()
 
+def run(module, params, use_diffusion, path, out_path, batch_size):
     tof_brain = nib.load(path)
     header, affine = tof_brain.header, tof_brain.affine
             
     tof_brain = jnp.array(tof_brain.get_fdata().astype(np.float32))
-    #tof_brain = vmap_transform(tof_brain) * 2 - 1
-    tof_brain = transform(tof_brain) * 2 - 1
     tof_brain = rearrange(tof_brain, "h w b -> b h w 1")
+    tof_brain = vmap_transform(tof_brain) * 2 - 1
     print("tof_brain:", tof_brain.shape, tof_brain.min(), tof_brain.max())
     num_slices, h, w, _ = tof_brain.shape
 
@@ -52,21 +65,25 @@ def main():
 
     # Batch image
     key = random.key(0)
-    num_iters = math.ceil(num_slices / 64)
+    num_iters = math.ceil(num_slices / batch_size)
     keys = random.split(key, num_iters*2)
 
     out_slices = []
     for i in tqdm(range(num_iters)):
-        start = i*64
-        if start + 64 >= num_slices:
+        start = i*batch_size
+        if start + batch_size >= num_slices:
             end = num_slices
         else:
-            end = start + 64
+            end = start + batch_size
         m = end - start
 
         img = random.normal(keys[i*2], (m, new_h, new_w, 1))
         tof_brain_slices = tof_brain[start:end]
-        out = sample(module=module, params=state.params, key=keys[i*2+1], img=img, condition=tof_brain_slices, num_sample_steps=100)
+
+        if use_diffusion:
+            out = sample(module=module, params=params, key=keys[i*2+1], img=img, condition=tof_brain_slices, num_sample_steps=100)
+        else:
+            out = module.apply(params, tof_brain_slices)
         out_slices.append(out)
     
     out = jnp.concatenate(out_slices, axis=0) # [-1,1]
@@ -79,12 +96,14 @@ def main():
     nib.save(img, out_path)
 
 if __name__ == "__main__":
-    #p = argparse.ArgumentParser(
-    #    description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    #)
-    #p.add_argument("--load", type=str, help="path to load pretrained weights from")
-    #p.add_argument("--bfloat16", action="store_true", help="use bfloat16 precision")
-    #p.add_argument("--input", type=str, help="path to image or list of images")
-    #p.add_argument("--output", type=str, help="output path")
-    #main(p.parse_args())
-    main()
+    p = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    p.add_argument("--load", type=str, help="path to load pretrained weights from")
+    p.add_argument("--bfloat16", action="store_true", help="use bfloat16 precision")
+    p.add_argument("--input", type=str, help="path to image or list of images")
+    p.add_argument("--arch", type=str, choices=["unet", "adm", "uvit"], help="architecture")
+    p.add_argument("--disable_diffusion", action="store_true", help="disable diffusion")
+    p.add_argument("--batch_size", type=int, default=64, help="how many slices to process in parallel")
+    p.add_argument("--output", type=str, help="output path", default="out.nii.gz")
+    main(p.parse_args())
